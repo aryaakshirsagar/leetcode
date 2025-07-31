74aec6e22fe0a5caced481ea1b03b41e1c564b91
aryaakshirsagar <thearyakshirsagar@gmail.com>
2025-07-31 15:25:57 +0530

    Time: 437 ms (57.74%), Space: 44.7 MB (69.64%) - LeetHub

---
+++ 0934-bitwise-ors-of-subarrays/0934-bitwise-ors-of-subarrays.py
@@ -0,0 +1,10 @@
+class Solution:
+    def subarrayBitwiseORs(self, arr):
+        res = set()
+        curr = set()
+        
+        for num in arr:
+            curr = {num | x for x in curr} | {num}
+            res |= curr
+        
+        return len(res)
