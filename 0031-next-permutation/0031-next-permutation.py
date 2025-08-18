7fe2634da9a67e29fe72bdc7541bfd1d60c0df9c
aryaakshirsagar <arya.kshirsagar.20230101039@siics.org>
2025-08-18 22:40:34 +0530

    Time: 0 ms (100%), Space: 12.4 MB (84.33%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,11 +1,25 @@
 class Solution:
-    def nextPermutation(self, nums: list[int]) -> None:
-        i = len(nums) - 2
+    def nextPermutation(self, nums):
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+
+        # 1) Find first index i from the right where nums[i] < nums[i+1]
+        i = n - 2
         while i >= 0 and nums[i] >= nums[i + 1]:
             i -= 1
+
+        # 2) If such i exists, find j from the right where nums[j] > nums[i] and swap
         if i >= 0:
-            j = len(nums) - 1
+            j = n - 1
             while nums[j] <= nums[i]:
                 j -= 1
             nums[i], nums[j] = nums[j], nums[i]
-        nums[i + 1:] = reversed(nums[i + 1:])
+
+        # 3) Reverse the suffix starting at i+1 (it’s in descending order)
+        l, r = i + 1, n - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
